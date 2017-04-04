1248fd6d9d1d3eabda6a779c1bb94aa6184210ef
Wlavell2016 <william.lavell@gmail.com>
2017-04-05 01:06:22 +0700

    nearly done

--- js/init.js
+++ js/init.js
@@ -1,7 +1,21 @@
-$(document).on('click', 'a', function(event){
-    event.preventDefault();
+// $(document).on('click', 'a', function(event){
+//     event.preventDefault();
+//
+//     $('html, body').animate({
+//         scrollTop: $( $.attr(this, 'href') ).offset().top
+//     }, 1200);
+//
+// });
 
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 1200);
+
+$('a[href*="#"]').click(function(e) {
+    e.preventDefault();
+    var target = this.hash;
+    $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 1200, 'swing', function () {
+        window.location.hash = target;
+    });
 });
