595fdc6f15925dd72303d4f89a48dd09e3c59c19
Wlavell2016 <william.lavell@gmail.com>
2017-04-05 22:57:27 +0700

    changed image sizes

--- js/init.js
+++ js/init.js
@@ -19,12 +19,3 @@ $('a[href*="#"]').click(function(e) {
         window.location.hash = target;
     });
 });
-
-
-var map;
-     function initMap() {
-       map = new google.maps.Map(document.getElementById('map'), {
-         center: {lat: -34.397, lng: 150.644},
-         zoom: 8
-       });
-     }
